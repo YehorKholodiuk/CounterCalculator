d791b470c9c73a260fda61af01eb90efd23f02eb
ASUS <Yehor Kholodiuk>
2021-09-19 16:52:33 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [numbers,setNumbers] = useState([1,2,3,4,5])
+  const plusMinusCounter = (value,index) => {
+  const newArr = numbers.map((el,i) => index === i? el + value:el)
+    setNumbers(newArr)
+  }
 
-export default App;
+  const moveUpDown = (index) =>{
+  const temporary = numbers[index]
+    const newArr = [...numbers]
+    newArr[index] = [index + 1]
+    newArr[index + 1] = temporary
+    setNumbers(newArr)
+  }
+
+  return (
+
+      <div className="App">
+        <h1>Calculator</h1>
+
+        {numbers.map((el, index) => <div key={index}>
+              <button onClick={() => plusMinusCounter(-3, index)}>-3</button>
+              <button onClick={() => plusMinusCounter(-2, index)}>-2</button>
+              <button onClick={() => plusMinusCounter(-1, index)}>-1</button>
+              {el}
+
+              <button onClick={() => plusMinusCounter(+1, index)}>+1</button>
+              <button onClick={() => plusMinusCounter(+2, index)}>+2</button>
+              <button onClick={() => plusMinusCounter(+3, index)}>+3</button>
+              {' '}
+
+              <button onClick={() => moveUpDown(1, index)} disabled={index === 0}>up</button>
+              <button onClick={() => moveUpDown(-1, index)} disabled={index === numbers.length - 1}>down</button>
+
+            </div>)}
+      </div>
+        );
+        }
+
+
+        export default App;
+
+
+
+
+
+
